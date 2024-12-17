653f4c5c1da6b2df3ba35e26318d8a914e7e6608
bhavind2015@gmail.com <bhavind2015@gmail.com>
2024-12-17 13:24:57 +0530

    Add project files.

---
+++ Scripts/Information/LoadPackingTech.js
@@ -0,0 +1,232 @@
+﻿
+//first add an event listener for page load
+document.addEventListener("DOMContentLoaded", get_json_data, false); // get_json_data is the function name that will fire on page load
+
+//this function is in the event listener and will execute on page load
+function get_json_data() {
+    hitTabs(1);
+}
+
+
+function hitTabs(tabnum) {
+    var json_url;
+    if (tabnum == 1) {
+        json_url = '../resources/outdoorporce.json';
+    }
+    else if (tabnum == 2)//wall tiles
+    {
+        json_url = '../resources/walltiles.json';
+    }
+    else if (tabnum == 3)//nano tiles
+    {
+        json_url = '../resources/nano.json';
+    }
+    else if (tabnum == 4)//porcelain tiles
+    {
+        json_url = '../resources/porcelain.json';
+    }
+    else if (tabnum == 5)//doublecharge tiles
+    {
+        json_url = '../resources/dblcharge.json';
+    }
+    else if (tabnum == 6)//floor tiles
+    {
+        json_url = '../resources/floortiles.json';
+    }
+    // Relative URL of external json file
+    //Build the XMLHttpRequest (AJAX Request)
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {//when a good response is given do this
+            var data = JSON.parse(this.responseText); // convert the response to a json object
+            append_json(data, tabnum);// pass the json object to the append_json function
+        }
+    }
+    //set the request destination and type
+    xmlhttp.open("GET", json_url, true);
+    //set required headers for the request
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    // send the request
+    xmlhttp.send(); // when the request completes it will execute the code in onreadystatechange section    
+}
+
+
+function append_json(object, tabnum) {
+    var table;
+    if (tabnum == 1) {//pocelain
+        //packing
+        table = document.getElementById('tblpackingporcdetail');
+        $(".rowPClass").remove();
+        for (var i = 0; i < object.packing.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowPClass');
+            tr.innerHTML = '<td>' + object.packing[i].SIZE + '</td>' +
+                '<td>' + object.packing[i].BOXWEIGHT + '</td>' +
+                '<td>' + object.packing[i].PCSBOX + '</td>' +
+                '<td>' + object.packing[i].SQMBOX + '</td>' +
+                '<td>' + object.packing[i].SQFTBOX + '</td>' +
+                '<td>' + object.packing[i].BOXPALLET + '</td>' +
+                '<td>' + object.packing[i].PALLETCTN + '</td>' +
+                '<td>' + object.packing[i].BOXESCTN + '</td>' +
+                '<td>' + object.packing[i].TOTALSQMCTN + '</td>' +
+                '<td>' + object.packing[i].CTNWEIGHT + '</td>';
+            table.appendChild(tr);
+        }
+
+        //for technical
+        table = document.getElementById('tbltechporcdetail');
+        $(".rowTClass").remove();
+        for (var i = 0; i < object.tech.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowTClass');
+            tr.innerHTML = '<td>' + object.tech[i].DESCRIPTION + '</td>' +
+                '<td>' + object.tech[i].ISO + '</td>' +
+                '<td>' + object.tech[i].MEAN + '</td>' +
+                '<td>' + object.tech[i].TESTMETHOD + '</td>';
+            table.appendChild(tr);
+        }
+    }
+    else if (tabnum == 2) {//walltiles
+        table = document.getElementById('tblpackingwalltiles');
+        $(".rowPClass").remove();
+        for (var i = 0; i < object.packing.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowPClass');
+            tr.innerHTML = '<td>' + object.packing[i].SIZE + '</td>' +
+                '<td>' + object.packing[i].WeightBox + '</td>' +
+                '<td>' + object.packing[i].TilesBox + '</td>' +
+                '<td>' + object.packing[i].SqMeterBox + '</td>' +
+                '<td>' + object.packing[i].PalletsContainer + '</td>' +
+                '<td>' + object.packing[i].BoxContainer + '</td>' +
+                '<td>' + object.packing[i].SqMeterContainer + '</td>';
+            table.appendChild(tr);
+        }
+
+        //for technical
+        table = document.getElementById('tbltechwalltiles');
+        $(".rowTClass").remove();
+        for (var i = 0; i < object.tech.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowTClass');
+            tr.innerHTML = '<td>' + object.tech[i].Properties + '</td>' +
+                '<td>' + object.tech[i].EuroStand + '</td>' +
+                '<td>' + object.tech[i].OurValue + '</td>';
+            table.appendChild(tr);
+        }
+    }
+    else if (tabnum == 3) {//nano
+        table = document.getElementById('tblpackingnano');
+        $(".rowPClass").remove();
+        for (var i = 0; i < object.packing.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowPClass');
+            tr.innerHTML = '<td>' + object.packing[i].SIZE + '</td>' +
+                '<td>' + object.packing[i].WeightBox + '</td>' +
+                '<td>' + object.packing[i].TilesBox + '</td>' +
+                '<td>' + object.packing[i].SqMeterBox + '</td>' +
+                '<td>' + object.packing[i].PalletsContainer + '</td>' +
+                '<td>' + object.packing[i].BoxContainer + '</td>' +
+                '<td>' + object.packing[i].SqMeterContainer + '</td>';
+            table.appendChild(tr);
+        }
+
+        //for technical
+        table = document.getElementById('tbltechnano');
+        $(".rowTClass").remove();
+        for (var i = 0; i < object.tech.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowTClass');
+            tr.innerHTML = '<td>' + object.tech[i].PROPERTIES + '</td>' +
+                '<td>' + object.tech[i].TESTMETHOD + '</td>' +
+                '<td>' + object.tech[i].ISO + '</td>' +
+                '<td>' + object.tech[i].OurValue + '</td>';
+            table.appendChild(tr);
+        }
+    }
+    else if (tabnum == 4) {//porcelain
+        table = document.getElementById('tblpackingporc');
+        $(".rowPClass").remove();
+        for (var i = 0; i < object.packing.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowPClass');
+            tr.innerHTML = '<td>' + object.packing[i].SIZE + '</td>' +
+                '<td>' + object.packing[i].WeightBox + '</td>' +
+                '<td>' + object.packing[i].TilesBox + '</td>' +
+                '<td>' + object.packing[i].SqMeterBox + '</td>' +
+                '<td>' + object.packing[i].PalletsContainer + '</td>' +
+                '<td>' + object.packing[i].BoxContainer + '</td>' +
+                '<td>' + object.packing[i].SqMeterContainer + '</td>';
+            table.appendChild(tr);
+        }
+
+        //for technical
+        table = document.getElementById('tbltechporc');
+        $(".rowTClass").remove();
+        for (var i = 0; i < object.tech.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowTClass');
+            tr.innerHTML = '<td>' + object.tech[i].PROPERTIES + '</td>' +
+                '<td>' + object.tech[i].TESTMETHOD + '</td>' +
+                '<td>' + object.tech[i].ISO + '</td>' +
+                '<td>' + object.tech[i].OurValue + '</td>';
+            table.appendChild(tr);
+        }
+    }
+    else if (tabnum == 5) {//dblcharge
+        table = document.getElementById('tblpackingdblchrg');
+        $(".rowPClass").remove();
+        for (var i = 0; i < object.packing.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowPClass');
+            tr.innerHTML = '<td>' + object.packing[i].SIZE + '</td>' +
+                '<td>' + object.packing[i].WeightBox + '</td>' +
+                '<td>' + object.packing[i].TilesBox + '</td>' +
+                '<td>' + object.packing[i].SqMeterBox + '</td>' +
+                '<td>' + object.packing[i].PalletsContainer + '</td>' +
+                '<td>' + object.packing[i].BoxContainer + '</td>' +
+                '<td>' + object.packing[i].SqMeterContainer + '</td>';
+            table.appendChild(tr);
+        }
+
+        //for technical
+        table = document.getElementById('tbltechdblchrg');
+        $(".rowTClass").remove();
+        for (var i = 0; i < object.tech.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowTClass');
+            tr.innerHTML = '<td>' + object.tech[i].PROPERTIES + '</td>' +
+                '<td>' + object.tech[i].TESTMETHOD + '</td>' +
+                '<td>' + object.tech[i].ISO + '</td>' +
+                '<td>' + object.tech[i].OurValue + '</td>';
+            table.appendChild(tr);
+        }
+    }
+    else if (tabnum == 6) {//floor tiles
+        table = document.getElementById('tblpackingflrtiles');
+        $(".rowPClass").remove();
+        for (var i = 0; i < object.packing.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowPClass');
+            tr.innerHTML = '<td>' + object.packing[i].SIZE + '</td>' +
+                '<td>' + object.packing[i].WeightBox + '</td>' +
+                '<td>' + object.packing[i].TilesBox + '</td>' +
+                '<td>' + object.packing[i].SqMeterBox + '</td>' +
+                '<td>' + object.packing[i].PalletsContainer + '</td>' +
+                '<td>' + object.packing[i].BoxContainer + '</td>' +
+                '<td>' + object.packing[i].SqMeterContainer + '</td>';
+            table.appendChild(tr);
+        }
+
+        //for technical
+        table = document.getElementById('tbltechflrtiles');
+        $(".rowTClass").remove();
+        for (var i = 0; i < object.tech.length; i++) {
+            var tr = document.createElement('tr');
+            tr.setAttribute('class', 'rowTClass');
+            tr.innerHTML = '<td>' + object.tech[i].PROPERTIES + '</td>' +
+                '<td>' + object.tech[i].ISO + '</td>' +
+                '<td>' + object.tech[i].OurValue + '</td>';
+            table.appendChild(tr);
+        }
+    }
+}
---
+++ Scripts/LoadJsonFunc/LoadLVTSpec.js
@@ -0,0 +1,91 @@
+﻿
+//first add an event listener for page load
+document.addEventListener("DOMContentLoaded", get_json_data, false); // get_json_data is the function name that will fire on page load
+
+
+function LoadSelectedCat(selectedcategory) {
+    var json_url = '../resources/outdoorprodspec.json';
+    // Relative URL of external json file
+    //Build the XMLHttpRequest (AJAX Request)
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {//when a good response is given do this
+            var data = JSON.parse(this.responseText); // convert the response to a json object            
+            loadallprods_json(data, selectedcategory);// pass the json object to the append_json function
+        }
+    }
+    //set the request destination and type
+    xmlhttp.open("GET", json_url, true);
+    //set required headers for the request
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    // send the request
+    xmlhttp.send(); // when the request completes it will execute the code in onreadystatechange section    
+}
+
+function loadallprods_json(obj, selcat) {
+    var divHtml = document.getElementById('divlist');
+    var startdivs = "<div class='container'>";
+    startdivs += "<div class='text-center mx-auto mb-5 wow fadeInUp' data-wow-delay='0.1s' style='max-width: 600px;'>";
+    startdivs += "<h1 class='display-5 mb-4'>OUTDOOR STONES</h1></div>";
+    startdivs += "<div class='row g-0 team-items'>";
+    
+    for (var i = 0; i < obj.prods.length; i++) {
+        startdivs += "<div class='col-lg-3 col-md-6 wow fadeInUp' data-wow-delay='0.1s'>";
+        startdivs += "<div class='team-item position relative'>";
+        startdivs += "<div class='position-relative'>";
+        startdivs += "<a class='elem' href='" + obj.prods[i].spec.IMG1BYSIZE + "' title='" + obj.prods[i].name + "' data-lcl-txt='20MM Outdoor Porcelain Tiles'  data-lcl-thumb='" + obj.prods[i].spec.IMG2BYSIZE +"'>";
+        startdivs += "<img class='img-fluid-products' src='" + obj.prods[i].spec.IMG2BYSIZE + "' alt=''></a></div>";
+        startdivs += "<div class='bg-light text-center p-4'><h3 class='mt-2'><a href='#' onclick='setspecdetail_json(" + JSON.stringify(obj.prods[i]) + ");'>" + obj.prods[i].name + "</a></h3></div></div>";
+        startdivs += "</div>";
+    }
+    startdivs += "</div></div></div>";    
+    divHtml.innerHTML = startdivs;
+    
+}
+
+function setspecdetail_json(obj) {    
+    var divstring;
+    divstring = "<div class='container'><div class='row g-5'>";
+    divstring += "<div class='col-lg-6 wow fadeIn' data-wow-delay='0.1s'>";
+    divstring += "<img class='img-fluid' src='" + obj.spec.IMG5BYSIZE +"' alt=''>";
+    divstring += "</div>";
+    divstring += "<div class='col-lg-6 wow fadeIn' data-wow-delay='0.5s'>";
+    divstring += "<div class='product-specification'><h2>Specification</h2>";
+    divstring += "<table id='tblspec' class='table table-bordered'><tbody>";
+    
+    divstring += "<tr><td>Size (mm): " + obj.spec.SIZE + "</td></tr>";
+    divstring += "<tr><td>Color: " + obj.spec.COLOR + "</td></tr>";
+    divstring += "<tr><td>Material: " + obj.spec.MATERIAL + "</td></tr>";
+    divstring += "<tr><td>Suitability: " + obj.spec.SUITABILITY + "</td></tr>";
+    divstring += "<tr><td>Thickness: " + obj.spec.THICKNESS + "</td></tr>";
+    divstring += "<tr><td>Tiles Type: " + obj.spec.TILESTYPE + "</td></tr>";
+    
+
+    divstring += "</tbody></table>";
+    divstring += "</div></div></div><br />";
+    /*alert(obj.spec.DIFFSIZES[0].SIZE1.IMAGEPATH);*/
+    divstring += "<div class='row g-0 team-items'>";
+    for (var i = 0; i < obj.spec.DIFFSIZES.length; i++) {
+        divstring += "<div class='col-lg-3 col-md-6 wow fadeInUp' data-wow-delay='0.3s'><div class='team-item position-relative'>";
+        divstring += "<div class='position-relative'>";
+        divstring += "<a class='elem' href='~/content/img/team-2-large.jpg' title='" + obj.name + "' data-lcl-txt='" + obj.spec.SIZE + "' data-lcl-thumb='~/Content/img/categories/O/1.jpg'>";
+        divstring += "<img class='img-fluid-products' src='" + obj.spec.DIFFSIZES[i].IMAGEPATH + "' alt=''>";
+        divstring += "</a>";
+        divstring += "</div>";
+        divstring += "<div class='bg-light text-center p-4'>";
+        divstring += "<h3 class='mt-2'>" + obj.spec.DIFFSIZES[i].SIZETEXT + "</h3>";
+        divstring += "</div>";
+        divstring += "</div>";
+        divstring += "</div>";
+    }
+    divstring += "</div>";
+
+
+    divstring += "</div>"
+    
+    div = document.getElementById('specdiv');
+    div.innerHTML = divstring;
+
+    $("#maindiv").toggle();
+    $("#lnkSelected").text(obj.name);
+}
